50d34259ed6b2a2bd5502bf5fc536cf701eccafb
Edson Rodrigues <edsonrodsilva@gmail.com>
2017-01-19 02:45:30 -0200

    ...

--- config/app.php
+++ config/app.php
@@ -168,6 +168,7 @@ return [
          */
 
         Prettus\Repository\Providers\RepositoryServiceProvider::class,
+        Collective\Html\HtmlServiceProvider::class,
 
         //
 
@@ -227,6 +228,9 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
 
     ],
 
